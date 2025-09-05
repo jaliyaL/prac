6fcbab5eb149c64c91d75d0951d558da821dad24
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-05 23:17:59 +0530

    add q1

--- set 01/Task 01/main.go
+++ set 01/Task 01/main.go
@@ -4,5 +4,11 @@ import "fmt"
 
 func main() {
 
+	/*
+	   	Problem 1: Thread-Safe Counter (15 min)
+	   •	Implement a thread-safe counter with methods Inc() and Value().
+	   •	Optional: Implement it using channels instead of mutex.
+	*/
+
 	fmt.Println("hi")
 }
