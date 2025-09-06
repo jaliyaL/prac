6ab5b24efdbddc8fb0f6f84438475ab1d982bdf9
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-06 09:12:55 +0530

    for loop  solution

--- set 01/Task 01/main.go
+++ set 01/Task 01/main.go
@@ -1,14 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/*
+   	Problem 1: Thread-Safe Counter (15 min)
+   •	Implement a thread-safe counter with methods Inc() and Value().
+   •	Optional: Implement it using channels instead of mutex.
+*/
+
+type Counter struct {
+	mu      sync.Mutex
+	counter int
+}
+
+func (c *Counter) Inc() {
+
+}
+
+// func (c *Counter) Value() {
+
+// }
 
 func main() {
 
-	/*
-	   	Problem 1: Thread-Safe Counter (15 min)
-	   •	Implement a thread-safe counter with methods Inc() and Value().
-	   •	Optional: Implement it using channels instead of mutex.
-	*/
+	var Counter int = 0
 
-	fmt.Println("hi")
+	start := time.Now()
+	for i := 0; i < 1000; i++ {
+		Counter++
+	}
+	fmt.Println(Counter)
+	elapsed := time.Since(start)
+	fmt.Println("Processed time, ", elapsed)
+
 }
