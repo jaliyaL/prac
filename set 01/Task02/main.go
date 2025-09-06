d7779620a839aae176e436cb56b85b344f4c1f46
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-06 23:46:16 +0530

    normal loop

--- set 01/Task02/main.go
+++ set 01/Task02/main.go
@@ -12,14 +12,17 @@ func main() {
 	for _, value := range urlArray {
 		resp, err := http.Get(value)
 		if err != nil {
-			defer func() {
-				if r := recover(); r != nil {
-					fmt.Println("recoverd from", r)
-				}
-			}()
-
+			// defer func() {
+			// 	if r := recover(); r != nil {
+			// 		fmt.Println("recoverd from", r)
+			// 	}
+			// }()
+			fmt.Printf("An error occurred: %v\n", err)
+			// The program will continue from here.
+			fmt.Println("Program did not panic and will continue.")
+			continue
 		}
+		defer resp.Body.Close()
 		fmt.Println("Response from -", value, resp.Status)
 	}
-
 }
