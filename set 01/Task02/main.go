2e43c98b54c0616a08092b0dbb4f9b0a3626152d
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-06 23:03:44 +0530

    code commit

--- set 01/Task02/main.go
+++ set 01/Task02/main.go
@@ -6,20 +6,20 @@ import (
 )
 
 func main() {
+	// This URL has a typo (.comm vs .com) and will result in an error.
+	url := "https://www.google.comm/"
 
-	urlArray := []string{"https://gobyexample.com", "https://www.google.comm/", "https://edition.cnn.com/"}
+	fmt.Printf("Attempting to fetch: %s\n", url)
+	resp, err := http.Get(url)
 
-	for _, value := range urlArray {
-		resp, err := http.Get(value)
-		if err != nil {
-			defer func() {
-				if r := recover(); r != nil {
-					fmt.Println("recoverd from", r)
-				}
-			}()
-
-		}
-		fmt.Println("Response from -", value, resp.Status)
+	// The program enters this block because err is not nil.
+	if err != nil {
+		fmt.Printf("An error occurred: %v\n", err)
+		// The program will continue from here.
+		fmt.Println("Program did not panic and will continue.")
+		return
 	}
 
+	defer resp.Body.Close()
+	fmt.Printf("Successfully fetched %s, Status: %s\n", url, resp.Status)
 }
