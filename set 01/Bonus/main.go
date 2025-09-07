56dde515e2e519c43725d61c93741ca647f597f5
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-07 15:49:21 +0530

    complete cache code

--- set 01/Bonus/main.go
+++ set 01/Bonus/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 /*  Implement a simple caching mechanism using a map and mutex. */
 
@@ -16,6 +19,36 @@ If multiple goroutines read/write a map at the same time, it will panic.
 Use sync.Mutex to lock the map while reading or writing.
 */
 
+type Cache struct {
+	mu   sync.Mutex
+	data map[string]int
+}
+
+func (c *Cache) setCache(name string, age int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.data[name] = age
+}
+
+func (c *Cache) getCache(name string) (int, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	age, ok := c.data[name]
+	return age, ok
+}
+
 func main() {
-	fmt.Println("hi")
+	c := Cache{data: map[string]int{}}
+	c.setCache("Namal", 50)
+	c.setCache("Sunil", 20)
+	c.setCache("Raj", 10)
+	c.setCache("Raju", 180)
+
+	if age, ok := c.getCache("Rajj"); ok {
+		fmt.Println(c.data)
+		fmt.Println(age)
+	} else {
+		fmt.Println("Not found")
+	}
+
 }
