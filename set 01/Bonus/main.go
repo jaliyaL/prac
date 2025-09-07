c60bae16f53fce237c648466357d932f7e27c4ae
jaliyaL <jaliyalamahewa@gmail.com>
2025-09-07 14:06:36 +0530

    setup

--- set 01/Bonus/main.go
+++ set 01/Bonus/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+/*  Implement a simple caching mechanism using a map and mutex. */
+
+/*
+🔑 Concepts
+Map for storage
+Go’s built-in map is perfect for storing key-value pairs.
+Example: map[string]string or map[string]int.
+
+Mutex for safety
+Maps in Go are not safe for concurrent access.
+If multiple goroutines read/write a map at the same time, it will panic.
+Use sync.Mutex to lock the map while reading or writing.
+*/
+
 func main() {
 	fmt.Println("hi")
 }
